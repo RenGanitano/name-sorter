7ed5585b90c4e4c56c55f943fdb1530fad2a7535
RenGanitano <renniel.ganitano@gmail.com>
2026-09-05 09:51:39 +0000

    Deploying to gh-pages from @ RenGanitano/name-sorter@0de55a5592cc399607145a5cad87d9c43835f776 🚀

--- main.js
+++ main.js
@@ -298,7 +298,7 @@ var assemblies = [
       { "name": "NameSorter.Services.FileNameReader", "rp": "name_sorter_FileNameReader.html", "cl": 5, "ucl": 0, "cal": 5, "tl": 17, "cb": 2, "tb": 2, "cm": 0, "fcm": 0, "tm": 0, "lch": [], "bch": [], "mch": [], "mfch": [], "hc": [], "metrics": { } },
       { "name": "NameSorter.Services.FileNameWriter", "rp": "name_sorter_FileNameWriter.html", "cl": 3, "ucl": 0, "cal": 3, "tl": 17, "cb": 0, "tb": 0, "cm": 0, "fcm": 0, "tm": 0, "lch": [], "bch": [], "mch": [], "mfch": [], "hc": [], "metrics": { } },
       { "name": "NameSorter.Services.NameParser", "rp": "name_sorter_NameParser.html", "cl": 9, "ucl": 0, "cal": 9, "tl": 29, "cb": 6, "tb": 6, "cm": 0, "fcm": 0, "tm": 0, "lch": [], "bch": [], "mch": [], "mfch": [], "hc": [], "metrics": { } },
-      { "name": "NameSorter.Services.NameSorterService", "rp": "name_sorter_NameSorterService.html", "cl": 6, "ucl": 0, "cal": 6, "tl": 23, "cb": 1, "tb": 2, "cm": 0, "fcm": 0, "tm": 0, "lch": [], "bch": [], "mch": [], "mfch": [], "hc": [], "metrics": { } },
+      { "name": "NameSorter.Services.NameSorterService", "rp": "name_sorter_NameSorterService.html", "cl": 6, "ucl": 0, "cal": 6, "tl": 23, "cb": 2, "tb": 2, "cm": 0, "fcm": 0, "tm": 0, "lch": [], "bch": [], "mch": [], "mfch": [], "hc": [], "metrics": { } },
     ]},
 ];
 
