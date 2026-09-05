66320fd7c7a481566d3238bbacc2b75d90110458
Ren Ganitano <renniel.ganitano@gmail.com>
2026-09-05 05:46:14 -0400

    add ConsoleNameWriterTests

---
+++ tests/NameSorter.Tests/ConsoleNameWriterTests.cs
@@ -0,0 +1,38 @@
+using AwesomeAssertions;
+using NameSorter.Models;
+using NameSorter.Services;
+
+namespace NameSorter.Tests;
+
+public class ConsoleNameWriterTests
+{
+    private static readonly object ConsoleOutputLock = new();
+
+    [Fact]
+    public void Write_ValidNames_OutputsToConsole()
+    {
+        lock (ConsoleOutputLock)
+        {
+            var originalOutput = Console.Out;
+
+            try
+            {
+                using var output = new StringWriter();
+                Console.SetOut(output);
+                var names = new List<PersonName>
+                {
+                    new(["Marin"], "Alvarez"),
+                    new(["Janet"], "Parsons"),
+                };
+
+                new ConsoleNameWriter().Write(names);
+
+                output.ToString().Should().Be($"Marin Alvarez{Environment.NewLine}Janet Parsons{Environment.NewLine}");
+            }
+            finally
+            {
+                Console.SetOut(originalOutput);
+            }
+        }
+    }
+}
